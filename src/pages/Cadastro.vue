52b86bac8c2ac35c94996539604f613b75f648aa
DGzzzzz <douglaswinter@outlook.com>
2025-02-14 21:58:17 -0300

    :sparkles: feat: connect api cadastro in front

--- src/pages/Cadastro.vue
+++ src/pages/Cadastro.vue
@@ -1,6 +1,29 @@
 <script setup lang="ts">
     import Button from '../components/Button/index.vue';
     import Input from '../components/Input/index.vue';
+
+    import axios from "axios";
+    import { ref } from 'vue';
+
+    const username = ref('');
+    const password = ref('');
+    const email = ref('');
+
+    const register = async () => {
+        try {
+            const response = await axios.post('http://localhost:8080/auth/register', {
+                username: username.value,
+                email: email.value,
+                password: password.value
+            });
+
+            console.log('Usuário cadastrado com sucesso: ');
+            alert('Cadastro realizado com sucesso!');
+        } catch (error) {
+            console.error('Erro ao cadastrar: ', error);
+            alert('Erro ao cadastrar usuário');
+        }
+    }
 </script>
 
 <template>
@@ -12,15 +35,18 @@
             <div class="container-inputs">
                 <h3>Bem-vindo</h3>
                 <h1>Faça o seu cadastro</h1>
+
                 <label for="name" id="labelNome">Nome</label>
-                <Input type="text" id="name" placeholder="Fulano de tal"></Input>
+                <Input type="text" id="name" placeholder="Fulano de tal" v-model="username"></Input>
+                
                 <label for="email" id="labelSenha">E-mail</label>
-                <Input type="email" id="email" placeholder="exemplo@gmail.com"></Input>
+                <Input type="email" id="email" placeholder="exemplo@gmail.com" v-model="email"></Input>
+                
                 <label for="senha" id="labelSenha">Senha</label>
-                <Input type="password" id="senha" placeholder="@Abc123"></Input>                
+                <Input type="password" id="senha" placeholder="@Abc123" v-model="password"></Input>                
             </div>
             <div class="container-btn">
-                <Button color="primary">Cadastrar</Button>
+                <Button color="primary" @click="register">Cadastrar</Button>
                 <div class="container-cad">
                     <router-link id="voltar" to="/"><Button color="secondary">Já tem conta? faça login</Button></router-link>
                 </div>
